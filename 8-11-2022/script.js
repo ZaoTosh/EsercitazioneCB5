9ec97a7eb7a441038c36a33c267b3d3e61a913be
Hysen Shemollari <hysen.shemollari@gmail.com>
2022-11-08 20:02:35 +0100

    code cleanup 8-11-2022

--- 8-11-2022/script.js
+++ 8-11-2022/script.js
@@ -1,10 +1,10 @@
 const divCard = document.querySelector(".card");
 
-function insertNum(n) {
+const insertNum = (n) => {
   if (n > 9 && n < 100) return "#0" + n;
   else if (n <= 9) return "#00" + n;
   else return "#" + n;
-}
+};
 //-------card--------
 const createCard = (image, num, namePoke, typePoke) => {
   const cardNode = document.createElement("div");
@@ -17,7 +17,7 @@ const createCard = (image, num, namePoke, typePoke) => {
   imgNode.setAttribute("src", image);
   imgNode.setAttribute("alt", typePoke);
 
-  const typeNode = document.createElement("h4");
+  const typeNode = document.createElement("p");
   typeNode.textContent = "Type: " + typePoke;
 
   const numNode = document.createElement("h3");
@@ -48,25 +48,10 @@ const colorPoke = {
   ghost: "#705898",
   ice: "#98d8d8",
 };
-//img, number, name, type
 
-//console.log(Object.values(object1).filter((item) => typeof item != "object"));
-//for (let i = 1; i < 150; i++)
-// fetch(`https://pokeapi.co/api/v2/pokemon/1`)
-//   .then((res) => res.json())
-//   .then((res) => res.map((item) => console.log(item)));
 for (let i = 1; i <= 150; i++) {
   let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
   let jsonPokemon = await pokemon.json();
-  // console.log(
-  //   jsonPokemon.sprites.other.dream_world.front_default +
-  //     " " +
-  //     jsonPokemon.id +
-  //     " " +
-  //     jsonPokemon.name +
-  //     " " +
-  //     jsonPokemon.types[0].type.name
-  // );
 
   createCard(
     jsonPokemon.sprites.other.dream_world.front_default,
@@ -75,4 +60,3 @@ for (let i = 1; i <= 150; i++) {
     jsonPokemon.types[0].type.name
   );
 }
-//let arrPokemon = jsonPokemon.map((item) => console.log(item.forms.id));
