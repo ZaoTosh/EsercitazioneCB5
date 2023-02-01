7f2b229bb0f4ae9204edfd252737f7b1bb80f922
Hysen Shemollari <hysen.shemollari@gmail.com>
2023-02-01 09:32:26 +0100

    Implemented Routes whitout objects

--- router-react-30-01-2023/src/index.js
+++ router-react-30-01-2023/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Root from "./routes/root";
 import Contacts from "./routes/contacts";
 import Contact from "./routes/contact";
@@ -10,34 +16,47 @@ import "./index.css";
 import ErrorPage from "./error-page";
 import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  // {
-  //   path: "/contacts",
-  //   element: <Root />,
-  // },
-  {
-    path: "/contacts/",
-    element: <Contacts />,
-  },
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//   },
+//   // {
+//   //   path: "/contacts",
+//   //   element: <Root />,
+//   // },
+//   {
+//     path: "/contacts/",
+//     element: <Contacts />,
+//   },
 
-  {
-    path: "/contacts/:contactID",
-    element: <Contact />,
-  },
-  {
-    path: "/posts/",
-    element: <Posts />,
-  },
-  {
-    path: "/posts/:postID",
-    element: <Post />,
-  },
-]);
+//   {
+//     path: "/contacts/:contactID",
+//     element: <Contact />,
+//   },
+//   {
+//     path: "/posts/",
+//     element: <Posts />,
+//   },
+//   {
+//     path: "/posts/:postID",
+//     element: <Post />,
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />} errorElement={<ErrorPage />} />
+      <Route path="/" element={<Root />} />
+      <Route path="/contacts" element={<Contacts />} />
+      <Route path="/contacts/:contactID" element={<Contact />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:postID" element={<Post />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
